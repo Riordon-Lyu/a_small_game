917150a3b4ee46ee6d3178db7eff9ef278756241
Riordon-Lyu <81798966+Riordon-Lyu@users.noreply.github.com>
2021-04-04 16:08:01 +0800

    Add files via upload

---
+++ test_for_stream.py
@@ -0,0 +1,69 @@
+import pandas as pd 
+import numpy as np 
+import streamlit as st 
+import time
+
+df=pd.read_excel('D:\迅雷下载\港股对标公司.xlsx')
+
+df
+
+st.write('This is my app!')
+
+st.write('''
+# hello world!
+this is my first app. Although I don\'t know the function of it for now, but it's would be a first small step that leads me into the internet world!
+''')
+
+x=st.slider('x')
+
+y=x+100
+
+y
+
+a=st.button('时间显示装置')
+
+tm=time.time()
+
+tm=time.localtime(tm)
+
+tm=time.strftime('%Y年%m月%d日 %H:%M:%S',tm)
+
+st.write(tm)
+
+title = st.text_input('Movie title', 'Life of Brian')
+
+if title=='Life of Brian':
+    st.write('The current movie title is', title)
+else:
+    st.write('')
+
+st.write('# 以下显示进度条')
+
+n=st.number_input('请输入完成进度情况（%）',0,100)
+my_bar = st.progress(n)
+
+b=st.button('放气球！')
+if b:
+    st.balloons()
+
+list=[1,2,3]
+    
+option = st.sidebar.selectbox('Which number do you like best?',list)
+
+if option==2:
+    st.balloons()
+    '你选择了正确的选项',option
+    st.slider('y')
+
+
+
+
+
+
+
+
+
+
+
+
+
